15ae610e4229273c2a939e971584d9dc8017cf1a
ttangNomad <tang.sbw@gmail.com>
2024-02-08 14:18:38 +0700

    ..

--- routes/mold/repair.js
+++ routes/mold/repair.js
@@ -5,15 +5,15 @@ const sql = require('mssql');
 const Redis = require('ioredis');
 const redis = new Redis();
 
-
+//TODO: StartTime, EndTime == ActualTime ??
 //* ========= Repair Issue =========
-router.post('/repair-issue', async (req, res) => { //TODO: PlanTime, Status
+router.post('/repair-issue', async (req, res) => { //TODO: Status
     try {
         let pool = await sql.connect(config);
         let { month, year, Status } = req.body;
 
         let repairIssue = await pool.request().query(`SELECT a.RepairCheckID, b.BasicMold, b.DieNo, a.RequestTime, a.StartTime, a.EndTime, a.Complaint,
-        a.RepairResult, a.ApproveBy, a.ReportNo
+        a.RepairResult, a.ApproveBy, a.ReportNo, a.PlanStartTime
         FROM [Mold].[RepairCheck] a
         LEFT JOIN [Mold].[MasterMold] b ON b.MoldID = a.MoldID
         WHERE MONTH(a.RequestTime) = ${month} AND YEAR(a.RequestTime) = ${year}
@@ -156,29 +156,32 @@ router.post('/repair-issue/repair/start', async (req, res) => {
         res.status(500).send({ message: `${err}` });
     }
 })
-router.post('/repair-issue/repair/item', async (req, res) => { //TODO:
+router.post('/repair-issue/repair/item', async (req, res) => {
     try {
         let pool = await sql.connect(config);
         let { RepairCheckID } = req.body;
 
         let repair = await pool.request().query(`SELECT a.RepairCheckID, a.RequestTime, a.RepairProblemID, a.RepairTypeID, a.Complaint,
-        a.StartTime, a.EndTime, a.RootCause, a.DetailOfRepair, a.RepairResult,
-        b.FirstName AS RequestSign, c.FirstName AS RepairBy, d.FirstName AS ApproveBy, e.FirstName AS ReceiveBy,
-        f.FirstName AS ReceiveApproveBy
+        a.StartTime, a.EndTime, a.DetailOfRepair, a.RepairResult, a.OccurTime, a.Attachment, a.RepairImagePath,
+        a.PlanStartTime, a.PlanFinishTime, a.ActualStartTime, a.ActualFinishTime, a.PlanActualArr,
+        b.FirstName AS RequestBy, c.FirstName AS RepairBy, d.FirstName AS ApproveBy,
+        e.FirstName AS InjCheckBy, f.FirstName AS QcCheckBy, g.FirstName AS MoldCheckBy
         FROM [Mold].[RepairCheck] a
         LEFT JOIN [TSMolymer_F].[dbo].[User] b ON a.RequestBy = b.EmployeeID
         LEFT JOIN [TSMolymer_F].[dbo].[User] c ON a.RepairBy = c.EmployeeID
         LEFT JOIN [TSMolymer_F].[dbo].[User] d ON a.ApproveBy = d.EmployeeID
-        LEFT JOIN [TSMolymer_F].[dbo].[User] e ON a.ReceiveBy = e.EmployeeID
-        LEFT JOIN [TSMolymer_F].[dbo].[User] f ON a.ReceiveApproveBy = f.EmployeeID
+        LEFT JOIN [TSMolymer_F].[dbo].[User] e ON a.InjCheckBy = e.EmployeeID
+        LEFT JOIN [TSMolymer_F].[dbo].[User] f ON a.QcCheckBy = f.EmployeeID
+        LEFT JOIN [TSMolymer_F].[dbo].[User] g ON a.MoldCheckBy = g.EmployeeID
         WHERE a.RepairCheckID = ${RepairCheckID};
         `);
         if(repair.recordset.length){
-            repair.recordset[0].RequestSign = !repair.recordset[0].RequestSign ? null: atob(repair.recordset[0].RequestSign);
+            repair.recordset[0].RequestBy = !repair.recordset[0].RequestBy ? null: atob(repair.recordset[0].RequestBy);
             repair.recordset[0].RepairBy = !repair.recordset[0].RepairBy ? null: atob(repair.recordset[0].RepairBy);
             repair.recordset[0].ApproveBy = !repair.recordset[0].ApproveBy ? null: atob(repair.recordset[0].ApproveBy);
-            repair.recordset[0].ReceiveBy = !repair.recordset[0].ReceiveBy ? null: atob(repair.recordset[0].ReceiveBy);
-            repair.recordset[0].ReceiveApproveBy = !repair.recordset[0].ReceiveApproveBy ? null: atob(repair.recordset[0].ReceiveApproveBy);
+            repair.recordset[0].InjCheckBy = !repair.recordset[0].InjCheckBy ? null: atob(repair.recordset[0].InjCheckBy);
+            repair.recordset[0].QcCheckBy = !repair.recordset[0].QcCheckBy ? null: atob(repair.recordset[0].QcCheckBy);
+            repair.recordset[0].MoldCheckBy = !repair.recordset[0].MoldCheckBy ? null: atob(repair.recordset[0].MoldCheckBy);
         }
         res.json(repair.recordset);
     } catch (err) {
@@ -199,17 +202,15 @@ router.post('/repair-issue/repair/process', async (req, res) => { // initial Pro
         res.status(500).send({ message: `${err}` });
     }
 })
-router.post('/repair-issue/repair/edit', async (req, res) => { //TODO: Plan & Actual
+router.post('/repair-issue/repair/plan-actual/edit', async (req, res) => {
     try {
         let pool = await sql.connect(config);
-        let { RepairCheckID, PlanStartTime, PlanFinishTime, ActualStartTime, ActualFinishTime, Plan } = req.body;
-
-        //RepairResult 1: Accepted, 2: Rejected, 3: Special Accept
-        let updateRepair = `UPDATE [Mold].[RepairCheck] SET DetailOfRepair = N'${DetailOfRepair}', RepairResult = ${RepairResult}
+        let { RepairCheckID, PlanStartTime, PlanFinishTime, ActualStartTime, ActualFinishTime, PlanActualArr } = req.body;
+        let updateRepair = `UPDATE [Mold].[RepairCheck] SET PlanStartTime = '${PlanStartTime}', PlanFinishTime = '${PlanFinishTime}',
+        ActualStartTime = '${ActualStartTime}', ActualFinishTime = '${ActualFinishTime}', PlanActualArr = '${PlanActualArr}'
         WHERE RepairCheckID = ${RepairCheckID};
         `;
         await pool.request().query(updateRepair);
-
         res.json({ message: 'Success' });
     } catch (err) {
         console.log(req.url, err);
@@ -232,6 +233,41 @@ router.post('/repair-issue/repair/detail/edit', async (req, res) => {
         console.log(req.url, err);
         res.status(500).send({ message: `${err}` });
     }
+})
+
+const storageRepairImage = multer.diskStorage({
+    destination: path.join(__dirname, '../../public/mold/repair'),
+    filename: (req, file, cb) => {
+        let uploadDate = new Date();
+        let uploadDateStr = `${uploadDate.getFullYear()}-${uploadDate.getMonth()+1}-${uploadDate.getDate()}_${uploadDate.getHours()}-${uploadDate.getMinutes()}-${uploadDate.getSeconds()}`;
+        const ext = file.mimetype.split('/')[1];
+        cb(null, `${uploadDateStr}` + '.' + ext);
+    }
+});
+const uploadRepairImage = multer({ storage: storageRepairImage }).single('repair_image');
+router.post('/repair-issue/repair/image/upload', async (req, res) => {
+    uploadRepairImage(req, res, async (err) => {
+        if (err) {
+            console.log(req.url, 'Upload ERROR', err);
+            res.status(500).send({ message: `${err}` });
+        } else {
+            try {
+                let pool = await sql.connect(config);
+                let { RepairCheckID } = req.body;
+                let ImagePath = (req.file) ? "/mold/repair/" + req.file.filename : ""
+
+                let updateImage = `UPDATE [Em].[RepairCheck] SET RepairImagePath = '${ImagePath}' WHERE RepairCheckID = ${RepairCheckID};`;
+                await pool.request().query(updateImage);
+
+                res.header('Access-Control-Allow-Origin', req.headers.origin);
+                res.header('Access-Control-Allow-Credentials', true);
+                res.status(200).send({ message: 'Success Upload File' });
+            } catch (err) {
+                console.log(req.url, 'DB ERROR', err);
+                res.status(500).send({ message: `${err}` });
+            }
+        }
+    })
 })
 
 // Tech
@@ -349,7 +385,7 @@ router.post('/repair-issue/service', async (req, res) => {
 router.post('/repair-issue/service/add', async (req, res) => { // ถ้า Use มากกว่า Remain
     try {
         let pool = await sql.connect(config);
-        let { RepairCheckID, SpareID, Qty, UnitPrice, Reuse } = req.body;
+        let { RepairCheckID, SpareID, Qty, UnitPrice } = req.body;
 
         // Get Remain
         let getRemain = await pool.request().query(`DECLARE @BF INT,
@@ -371,8 +407,8 @@ router.post('/repair-issue/service/add', async (req, res) => { // ถ้า Use 
         if(Qty > remain) return res.status(400).send({ message: 'คงเหลือใน Stock น้อยกว่าที่ใช้' });
 
 
-        let insertPart = `INSERT [Mold].[RepairCost](RepairCheckID, SpareID, Qty, UnitPrice, UsedDate, Reuse)
-        VALUES(${RepairCheckID}, ${SpareID}, ${Qty}, ${UnitPrice}, GETDATE(), ${Reuse});
+        let insertPart = `INSERT [Mold].[RepairCost](RepairCheckID, SpareID, Qty, UnitPrice, UsedDate)
+        VALUES(${RepairCheckID}, ${SpareID}, ${Qty}, ${UnitPrice}, GETDATE());
         `;
         await pool.request().query(insertPart);
 
