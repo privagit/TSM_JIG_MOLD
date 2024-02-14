3fda7729869d54d188cbc4049ca1fb1e49ca46c7
jrpaof <jirapat.ph@ku.th>
2024-02-14 10:33:51 +0700

    Update pool-manager.js

--- middlewares/pool-manager.js
+++ middlewares/pool-manager.js
@@ -4,26 +4,26 @@ const pools = new Map();
 module.exports = {
     getPool: (name, config) => {
         if (!pools.has(name)) {
-         if (!config) {
-          throw new Error('Pool does not exist');
-         }
-         const pool = new mssql.ConnectionPool(config);
-         // automatically remove the pool from the cache if `pool.close()` is called
-         const close = pool.close.bind(pool);
-         pool.close = (...args) => {
-          pools.delete(name);
-          return close(...args);
-         }
-         pools.set(name, pool.connect());
+            if (!config) {
+                throw new Error('Pool does not exist');
+            }
+            const pool = new mssql.ConnectionPool(config);
+            // automatically remove the pool from the cache if `pool.close()` is called
+            const close = pool.close.bind(pool);
+            pool.close = (...args) => {
+                pools.delete(name);
+                return close(...args);
+            }
+            pools.set(name, pool.connect());
         }
         return pools.get(name);
-       },
-       /**
-        * Closes all the pools and removes them from the store
-        *
-        * @return {Promise<mssql.ConnectionPool[]>}
-        */
-       closeAllPool: () => Promise.all(Array.from(pools.values()).map((connect) => {
+    },
+    /**
+     * Closes all the pools and removes them from the store
+     *
+     * @return {Promise<mssql.ConnectionPool[]>}
+     */
+    closeAllPool: () => Promise.all(Array.from(pools.values()).map((connect) => {
         return connect.then((pool) => pool.close());
-       })),
+    })),
 };
