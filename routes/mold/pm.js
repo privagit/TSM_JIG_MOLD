e78b9da9ac11dd75ece30f297c107f3de7aad53b
ttangNomad <tang.sbw@gmail.com>
2024-02-28 15:19:46 +0700

    update PmMold edit Repair Pm Request

--- routes/mold/pm.js
+++ routes/mold/pm.js
@@ -4,31 +4,41 @@ const config = require('../../lib/dbconfig').dbconfig_mold;
 const { getPool } = require('../../middlewares/pool-manager');
 
 //* ========== PM ==========
-router.post('/', async (req, res) => { //TODO: RepairRequest, Condition AcceptStatus
+router.post('/', async (req, res) => { //TODO: Condition AcceptStatus
     try {
         let pool = await getPool('MoldPool', config);
         let { Status } = req.body;
         // Status 1: Issue, 2: Cancel, 3: Reject, 4: Accept
-        let pmList = await pool.request().query(`SELECT a.MoldID, a.BasicMold, a.DieNo, b.WarningShot, b.DangerShot, b.WarrantyWarningShot, b.WarrantyDangerShot, b.AlertPercent, b.AlertWarrantyPercent
+        let pmList = await pool.request().query(`SELECT a.MoldID, a.BasicMold, a.DieNo, b.WarningShot, b.DangerShot, b.WarrantyWarningShot, b.WarrantyDangerShot, b.AlertPercent, b.AlertWarrantyPercent,
+        c.ActualPmShot, c.ActualWarrantyShot
         FROM [Mold].[MasterMold] a
         LEFT JOIN [Mold].[MasterPm] b ON b.MoldID = a.MoldID
+        LEFT JOIN [Mold].[MoldShot] c ON c.MoldID = a.MoldID
         WHERE a.Active = 1;
         `);
         let pmRequest = await pool.request().query(`WITH PmPlan AS (
             SELECT ROW_NUMBER() OVER (PARTITION BY a.MoldID, a.PmType ORDER BY a.RequestTime DESC) AS RowNum,
-            a.MoldID, a.PmType, a.AcceptStatus, a.PlanTime
+            a.MoldID, a.PmType, a.AcceptStatus, a.PlanStartTime
             FROM [Mold].[PmPlan] a
             WHERE a.PmEnd IS NULL
         )
-        SELECT MoldID, PmType, AcceptStatus, PlanTime, ROW_NUMBER() OVER (ORDER BY PlanTime) AS RowNo
+        SELECT MoldID, PmType, AcceptStatus, PlanStartTime, ROW_NUMBER() OVER (ORDER BY PlanStartTime) AS RowNo
         FROM [PmPlan]
         WHERE RowNum = 1;
         `);
-        //TODO:
-        let repairRequest = await pool.request().query(``);
-
-        if(Status){ //TODO:
-        }
+        let repairRequest = await pool.request().query(`WITH Repair AS (
+            SELECT ROW_NUMBER() OVER (PARTITION BY a.MoldID ORDER BY a.RequestTime DESC) AS RowNum,
+            a.MoldID, a.RepairCheckID, a.RepairStatus, a.AcceptStatus, a.PlanStartTime,
+            ISNULL(c.RepairProblem, b.RepairType) AS RepairProblem
+            FROM [Mold].[RepairCheck] a
+            LEFT JOIN [Mold].[MasterRepairType] b ON b.RepairTypeID = a.RepairTypeiD
+	        LEFT JOIN [Mold].[MasterRepairProblem] c ON c.RepairProblemID = a.RepairProblemID
+            WHERE a.EndTime IS NULL
+        )
+        SELECT MoldID, RepairCheckID, RepairStatus, AcceptStatus, PlanStartTime, ROW_NUMBER() OVER (ORDER BY PlanStartTime) AS RowNo, RepairProblem
+        FROM [Repair]
+        WHERE RowNum = 1;
+        `);
 
         let moldArr = [];
         for(let mold of pmList.recordset){
@@ -50,16 +60,40 @@ router.post('/', async (req, res) => { //TODO: RepairRequest, Condition AcceptSt
                 }
 
                 mold.minPlanTimeNo = minPlanTimeNo;
-                mold.PmStatus = !pm.length ? (mold.MonthlyShot >= alertMonthly ? 0 : null) : pm[0].AcceptStatus;
-                mold.WarrantyStatus = !warranty.length ? (mold.MonthlyShot >= alertMonthly ? 0 : null) : warranty[0].AcceptStatus;
+                mold.PmStatus = !pm.length ? (mold.MonthlyShot >= alertPm ? 0 : null) : pm[0].AcceptStatus;
+                mold.WarrantyStatus = !warranty.length ? (mold.MonthlyShot >= alertWarranty ? 0 : null) : warranty[0].AcceptStatus;
 
                 needPM = true;
+            }
+
+            let repairFiltered = repairRequest.recordset.filter(v => v.MoldID == mold.MoldID);
+            if(repairFiltered.length){
+                mold.RepairCheckID = repairFiltered[0].RepairCheckID;
+                mold.RepairStatus = repairFiltered[0].RepairStatus;
+                mold.AcceptStatus = repairFiltered[0].AcceptStatus;
+                mold.PlanStartTime = repairFiltered[0].PlanStartTime;
+                mold.RepairStatus = repairFiltered[0].RepairStatus;
+                mold.RepairProblem = repairFiltered[0].RepairProblem;
+
+                needPM = true;
+            } else{
+                mold.RepairCheckID = null;
+                mold.RepairStatus = null;
+                mold.AcceptStatus = null;
+                mold.PlanStartTime = null;
+                mold.RepairStatus = null;
+                mold.RepairProblem = null;
             }
 
             if(needPM){
                 moldArr.push(mold);
             }
         }
+
+        //TODO:
+        if(Status){
+        }
+
         res.json(pmList.recordset);
     } catch (err) {
         console.log(req.url, err);
@@ -67,7 +101,7 @@ router.post('/', async (req, res) => { //TODO: RepairRequest, Condition AcceptSt
     }
 })
 
-router.post('/pm/item', async (req, res) => { //TODO: Filter PmRequest
+router.post('/pm/item', async (req, res) => { //? Filter PmRequest
     try {
         let pool = await getPool('MoldPool', config);
         let { MoldID } = req.body;
@@ -111,23 +145,42 @@ router.post('/pm/item', async (req, res) => { //TODO: Filter PmRequest
         res.status(500).send({ message: `${err}` });
     }
 })
-router.post('/pm/request', async (req, res) => { //TODO: request PM
+router.post('/pm/request', async (req, res) => { // request PM
     try {
         let pool = await getPool('MoldPool', config);
-        
+        let { MoldID, PmList, PlanStartTime, RequestBy } = req.body;
+        let PlanTime = new Date(PlanStartTime);
+        let FinishTime = new Date(PlanStartTime);
+
+        // PmList = [{PmType, Actual, PmTime, Remark}]
+        for(let item of PmList){
+            FinishTime.setMinutes(PlanTime.getMinutes() + item.PmTime);
+            let PlanTimeStr = `${PlanTime.getFullYear()}-${PlanTime.getMonth()+1}-${PlanTime.getDate()} ${PlanTime.getHours()}:${PlanTime.getMinutes()}:${PlanTime.getSeconds()}`;
+            let FinishTimeStr = `${FinishTime.getFullYear()}-${FinishTime.getMonth()+1}-${FinishTime.getDate()} ${FinishTime.getHours()}:${FinishTime.getMinutes()}:${FinishTime.getSeconds()}`;
+
+            // Insert PmPlan
+            // PlanStatus = 0: Wait Accept, 1: Accept, 2: Reject
+            let insertPlan = `INSERT INTO [Mold].[PmPlan](MoldID, PlanStartTime, PlanFinishTime, PmTime, PmType, ActualShot, Remark, RequestBy, RequestTime, AcceptStatus)
+            VALUES(${MoldID}, '${PlanTimeStr}', '${FinishTimeStr}', ${item.PmTime}, ${item.PmType}, ${item.Actual}, N'${item.Remark}', ${RequestBy}, GETDATE(), 0);
+            `;
+            await pool.request().query(insertPlan);
+            PlanTime = FinishTime; // set next start plan time
+        }
+
+        res.json({ message: 'Success' });
     } catch (err) {
         console.log(req.url, err);
         res.status(500).send({ message: `${err}` });
     }
 })
-router.post('/repair/item', async (req, res) => { // repair list
+router.post('/repair/item', async (req, res) => { // repair list that Status = issue
     try {
         let pool = await getPool('MoldPool', config);
         let { MoldID } = req.body;
         let repairList = await pool.request().query(`SELECT a.RepairCheckID, a.RequestTime, b.RepairProblem
         FROM [Mold].[RepairCheck] a
         LEFT JOIN [Mold].[MasterRepairProblem] b ON b.RepairProblemID = a.RepairProblemID
-        WHERE a.MoldID = ${MoldID};
+        WHERE a.MoldID = ${MoldID} AND a.RepairStatus = 1;
         `);
         res.json(repairList.recordset);
     } catch (err) {
@@ -136,6 +189,34 @@ router.post('/repair/item', async (req, res) => { // repair list
     }
 })
 router.post('/repair/request', async (req, res) => { // request Repair
+    try {
+        let pool = await getPool('MoldPool', config);
+        let { RepairList, PlanStartTime } = req.body;
+        let PlanTime = new Date(PlanStartTime);
+        let FinishTime = new Date(PlanStartTime);
+
+        // RepairList = [{RepairCheckID, EstTime}]
+        for(let item of RepairList){
+            FinishTime.setMinutes(PlanTime.getMinutes() + item.EstTime);
+            let PlanTimeStr = `${PlanTime.getFullYear()}-${PlanTime.getMonth()+1}-${PlanTime.getDate()} ${PlanTime.getHours()}:${PlanTime.getMinutes()}:${PlanTime.getSeconds()}`;
+            let FinishTimeStr = `${FinishTime.getFullYear()}-${FinishTime.getMonth()+1}-${FinishTime.getDate()} ${FinishTime.getHours()}:${FinishTime.getMinutes()}:${FinishTime.getSeconds()}`;
+
+            // Insert RepairPlan
+            // PlanStatus = 0: Wait Accept, 1: Accept, 2: Reject
+            let insertPlan = `INSERT INTO [Mold].[RepairPlan](RepairCheckID, PlanStartTime, PlanFinishTime, PlanStatus)
+            VALUES(${item.RepairCheckID}, '${PlanTimeStr}', '${FinishTimeStr}', 0);
+            `;
+            await pool.request().query(insertPlan);
+            PlanTime = FinishTime; // set next start plan time
+        }
+
+        res.json({ message: 'Success' });
+    } catch (err) {
+        console.log(req.url, err);
+        res.status(500).send({ message: `${err}` });
+    }
+})
+router.post('/repair/request/v1', async (req, res) => { //! Deprecated: request Repair
     try {
         let pool = await getPool('MoldPool', config);
         let { RepairList, PlanStartTime } = req.body;
@@ -184,8 +265,7 @@ router.post('/repair/request', async (req, res) => { // request Repair
         res.status(500).send({ message: `${err}` });
     }
 })
-
-router.post('/repair/process', async (req, res) => { //TODO: initial Process
+router.post('/repair/process', async (req, res) => { //! Deprecated: initial Process
     try {
         let pool = await getPool('MoldPool', config);
         let { MoldID } = req.body;
